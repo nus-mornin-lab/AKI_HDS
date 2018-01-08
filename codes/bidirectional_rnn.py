44243aac3e24f328f811ac779748a8398ef14a17
Max Poon <maxpanziyuan@gmail.com>
2018-01-08 10:28:48 +0800

    bugfix

--- codes/bidirectional_rnn.py
+++ codes/bidirectional_rnn.py
@@ -1,31 +1,35 @@
-import  tensorflow as tf
+import tensorflow as tf
 from tensorflow.contrib import rnn
 from data_iterator import DataIterator
 
 
-numFeatures = 29
+numFeatures = 60
 batchSize = 64
 stateSize = 64
 
 def buildGraph():
 	x = tf.placeholder(tf.float64, [batchSize, None, numFeatures])  # [batchSize, num_steps, numFeatures]
 	sequenceLengths = tf.placeholder(tf.int32, [batchSize]) # [batchSize]
-	y = tf.placeholder(tf.int32, [batchSize, None]) # [batchSize, num_steps]
-	musk = tf.placeholder(tf.int32, [batchSize, None]) # [batchSize, num_steps]
+	y = tf.placeholder(tf.float64, [batchSize, None]) # [batchSize, num_steps]
+	musk = tf.placeholder(tf.float64, [batchSize, None]) # [batchSize, num_steps]
 	# Forward direction cell
-	lstmFwCell = rnn.BasicLSTMCell(stateSize, forget_bias=1.0)
+	lstmFwCell = rnn.BasicLSTMCell(stateSize, forget_bias=1.0, activation=tf.nn.relu)
 	# Backward direction cell
-	lstmBwCell = rnn.BasicLSTMCell(stateSize, forget_bias=1.0)
-	outputs, states = tf.nn.bidirectional_dynamic_rnn(lstmFwCell, lstmBwCell, x, sequenceLengths)
+	lstmBwCell = rnn.BasicLSTMCell(stateSize, forget_bias=1.0, activation=tf.nn.relu)
+	outputs, states = tf.nn.bidirectional_dynamic_rnn(lstmFwCell, lstmBwCell, x, sequenceLengths, dtype=tf.float64)
+	outputsFw, outputsBw = outputs
+	outputs = tf.concat([outputsFw, outputsBw], axis=2)
+	outputs = tf.reshape(outputs, [-1, stateSize*2])
 	with tf.variable_scope('softmax'):
-		W = tf.get_variable('W', [stateSize], initializer=tf.contrib.layers.xavier_initializer())
-		b = tf.get_variable('b', [1], initializer=tf.constant_initializer(0.0))
+		W = tf.get_variable('W', [stateSize*2, 1], initializer=tf.contrib.layers.xavier_initializer(), dtype=tf.float64)
+		b = tf.get_variable('b', [1], initializer=tf.constant_initializer(0.0), dtype=tf.float64)
 	logits = tf.matmul(outputs, W) + b # [batchSize, num_steps]
 	predicts = tf.nn.sigmoid(logits)
-	losses = -y*tf.log(predicts) - (1-y)*tf.log(1-predicts)
+	predicts = tf.reshape(predicts, [batchSize, -1])
+	losses = -y*tf.log(tf.clip_by_value(predicts, 1e-10, 1.0)) - (1-y)*tf.log(tf.clip_by_value(1-predicts, 1e-10, 1.0))
 	losses *= musk
 	loss = tf.reduce_mean(tf.reduce_sum(losses, reduction_indices=[1]))
-	accuracy = tf.reduce_sum(tf.abs(predicts-y)*musk < 0.5)/tf.reduce_sum(musk)
+	accuracy = tf.reduce_sum(tf.cast(tf.abs(predicts-y) < 0.5, tf.float64)*musk)/tf.reduce_sum(musk)
 	trainStep = tf.train.AdamOptimizer(1e-4).minimize(loss)
 	return {
 		'x': x,
@@ -38,36 +42,34 @@ def buildGraph():
 	}
 
 
-def trainGraph(g, train, test, epochs=10):
-	with tf.Session() as sess:
-		sess.run(tf.initialize_all_variables())
-		tr = DataIterator(train)
-		te = DataIterator(test)
+def trainGraph(g, sess, train, test, epochs=10):
+	tr = DataIterator(train)
+	te = DataIterator(test)
 
-		step, accuracy = 0, 0
-		trLosses, teLosses = [], []
-		current_epoch = 0
-		while current_epoch < epochs:
-			step += 1
-			batch = tr.next_batch(batchSize)
-			feed = {g['x']: batch[0], g['y']: batch[1], g['seqlen']: batch[2]}
-			accuracy_, _ = sess.run([g['accuracy'], g['trainStep']], feed_dict=feed)
-			accuracy += accuracy_
+	step, accuracy = 0, 0
+	trLosses, teLosses = [], []
+	current_epoch = 0
+	while current_epoch < epochs:
+		step += 1
+		batch = tr.next_batch(batchSize)
+		feed = {g['x']: batch[0], g['y']: batch[1], g['seqlen']: batch[2], g['musk']: batch[3]}
+		accuracy_, _ = sess.run([g['accuracy'], g['trainStep']], feed_dict=feed)
+		accuracy += accuracy_
 
-			if tr.epochs > current_epoch:
-				current_epoch += 1
-				trLosses.append(accuracy / step)
-				step, accuracy = 0, 0
+		if tr.epochs > current_epoch:
+			current_epoch += 1
+			trLosses.append(accuracy / step)
+			step, accuracy = 0, 0
 
-				# eval test set
-				teEpoch = te.epochs
-				while te.epochs == teEpoch:
-					step += 1
-					batch = te.next_batch(batchSize)
-					feed = {g['x']: batch[0], g['y']: batch[1], g['seqlen']: batch[2]}
-					accuracy_ = sess.run([g['accuracy']], feed_dict=feed)[0]
-					accuracy += accuracy_
+			# eval test set
+			teEpoch = te.epochs
+			while te.epochs == teEpoch:
+				step += 1
+				batch = te.next_batch(batchSize)
+				feed = {g['x']: batch[0], g['y']: batch[1], g['seqlen']: batch[2], g['musk']: batch[3]}
+				accuracy_ = sess.run([g['accuracy']], feed_dict=feed)[0]
+				accuracy += accuracy_
 
-				teLosses.append(accuracy / step)
-				step, accuracy = 0, 0
-				print("Accuracy after epoch", current_epoch, " - tr:", trLosses[-1], "- te:", teLosses[-1])
+			teLosses.append(accuracy / step)
+			step, accuracy = 0, 0
+			print("Accuracy after epoch", current_epoch, " - tr:", trLosses[-1], "- te:", teLosses[-1])
--- codes/data_iterator.py
+++ codes/data_iterator.py
@@ -8,12 +8,15 @@ class DataIterator:
 		self.epochs = 0
 		self.cursor = np.array([0] * numBuckets)
 		timeSeriesList.sort(key=lambda x: len(x))
-		self.sequenceLengths = [len(timeSeries) for timeSeries in timeSeriesList]
+		self.sequenceLengths = np.array([len(timeSeries) for timeSeries in timeSeriesList])
 		# add padding
 		maxLength = len(max(timeSeriesList, key=lambda x: len(x)))
 		self.data = np.zeros([len(timeSeriesList), maxLength, len(timeSeriesList[0].columns)], dtype=np.float64)
 		for i, data_i in enumerate(self.data):
 			data_i[:self.sequenceLengths[i]] = timeSeriesList[i].values
+		self.musks = np.zeros([len(timeSeriesList), maxLength], dtype=np.int32)
+		for i, musk in enumerate(self.musks):
+			musk[:self.sequenceLengths[i]] = np.ones(self.sequenceLengths[i])
 
 	def next_batch(self, n):
 		if np.any(self.cursor + n + 1 > self.size):
@@ -21,9 +24,10 @@ class DataIterator:
 			self.cursor -= self.cursor
 		i = np.random.randint(0, self.numBuckets)
 		index = min(self.cursor[i]+n, self.size)
-		maxLength = max(self.sequenceLengths[self.cursor:index])
+		maxLength = max(self.sequenceLengths[self.cursor[i]:index])
 		x = self.data[self.cursor[i]:index, :maxLength, :-1]
 		y = self.data[self.cursor[i]:index, :maxLength, -1]
 		sequenceLengths = self.sequenceLengths[self.cursor[i]:index]
+		musks = self.musks[self.cursor[i]:index, :maxLength]
 		self.cursor[i] += n
-		return x, y, sequenceLengths
+		return x, y, sequenceLengths, musks
