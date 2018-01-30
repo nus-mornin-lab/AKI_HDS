68615b80eb5ddc03529d09c9981dcf512f146b94
Max Poon <maxpanziyuan@gmail.com>
2018-01-30 15:28:34 +0800

    use only one cursor

--- codes/data_iterator.py
+++ codes/data_iterator.py
@@ -6,11 +6,11 @@ class DataIterator:
         self.size = len(timeSeriesList)
         self.numBuckets = numBuckets
         self.epochs = 0
-        self.cursor = np.array([0] * numBuckets)
+        self.cursor = 0
         timeSeriesList.sort(key=lambda x: len(x))
         self.sequenceLengths = np.array([len(timeSeries) for timeSeries in timeSeriesList])
         # add padding
-        maxLength = len(max(timeSeriesList, key=lambda x: len(x)))
+        maxLength = len(timeSeriesList[-1])
         self.data = np.zeros([len(timeSeriesList), maxLength, len(timeSeriesList[0].columns)], dtype=np.float64)
         for i, data_i in enumerate(self.data):
             data_i[:self.sequenceLengths[i]] = timeSeriesList[i].values
@@ -20,15 +20,14 @@ class DataIterator:
                 [1 if self.data[i, j, -1] == 0 else ratio for j in range(self.sequenceLengths[i])])
 
     def next_batch(self, n):
-        if np.any(self.cursor + n + 1 > self.size):
+        index = min(self.cursor + n, self.size)
+        maxLength = max(self.sequenceLengths[self.cursor:index])
+        x = self.data[self.cursor:index, :maxLength, :-1]
+        y = self.data[self.cursor:index, :maxLength, -1]
+        sequenceLengths = self.sequenceLengths[self.cursor:index]
+        musks = self.musks[self.cursor:index, :maxLength]
+        self.cursor += n
+        if self.cursor >= self.size:
             self.epochs += 1
-            self.cursor -= self.cursor
-        i = np.random.randint(0, self.numBuckets)
-        index = min(self.cursor[i] + n, self.size)
-        maxLength = max(self.sequenceLengths[self.cursor[i]:index])
-        x = self.data[self.cursor[i]:index, :maxLength, :-1]
-        y = self.data[self.cursor[i]:index, :maxLength, -1]
-        sequenceLengths = self.sequenceLengths[self.cursor[i]:index]
-        musks = self.musks[self.cursor[i]:index, :maxLength]
-        self.cursor[i] += n
+            self.cursor = 0
         return x, y, sequenceLengths, musks
