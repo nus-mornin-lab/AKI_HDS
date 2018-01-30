7e33193bfba6c3dab1fbe3abfe3b15712d67b713
Max Poon <maxpanziyuan@gmail.com>
2018-01-30 15:13:15 +0800

    using space

--- codes/data_iterator.py
+++ codes/data_iterator.py
@@ -2,32 +2,33 @@ import numpy as np
 
 
 class DataIterator:
-	def __init__(self, timeSeriesList, ratio=7.4, numBuckets=5):
-		self.size = len(timeSeriesList)
-		self.numBuckets = numBuckets
-		self.epochs = 0
-		self.cursor = np.array([0] * numBuckets)
-		timeSeriesList.sort(key=lambda x: len(x))
-		self.sequenceLengths = np.array([len(timeSeries) for timeSeries in timeSeriesList])
-		# add padding
-		maxLength = len(max(timeSeriesList, key=lambda x: len(x)))
-		self.data = np.zeros([len(timeSeriesList), maxLength, len(timeSeriesList[0].columns)], dtype=np.float64)
-		for i, data_i in enumerate(self.data):
-			data_i[:self.sequenceLengths[i]] = timeSeriesList[i].values
-		self.musks = np.zeros([len(timeSeriesList), maxLength], dtype=np.int32)
-		for i, musk in enumerate(self.musks):
-			musk[:self.sequenceLengths[i]] = np.array([1 if self.data[i, j, -1] == 0 else ratio for j in range(self.sequenceLengths[i])])
+    def __init__(self, timeSeriesList, ratio=7.4, numBuckets=5):
+        self.size = len(timeSeriesList)
+        self.numBuckets = numBuckets
+        self.epochs = 0
+        self.cursor = np.array([0] * numBuckets)
+        timeSeriesList.sort(key=lambda x: len(x))
+        self.sequenceLengths = np.array([len(timeSeries) for timeSeries in timeSeriesList])
+        # add padding
+        maxLength = len(max(timeSeriesList, key=lambda x: len(x)))
+        self.data = np.zeros([len(timeSeriesList), maxLength, len(timeSeriesList[0].columns)], dtype=np.float64)
+        for i, data_i in enumerate(self.data):
+            data_i[:self.sequenceLengths[i]] = timeSeriesList[i].values
+        self.musks = np.zeros([len(timeSeriesList), maxLength], dtype=np.int32)
+        for i, musk in enumerate(self.musks):
+            musk[:self.sequenceLengths[i]] = np.array(
+                [1 if self.data[i, j, -1] == 0 else ratio for j in range(self.sequenceLengths[i])])
 
-	def next_batch(self, n):
-		if np.any(self.cursor + n + 1 > self.size):
-			self.epochs += 1
-			self.cursor -= self.cursor
-		i = np.random.randint(0, self.numBuckets)
-		index = min(self.cursor[i]+n, self.size)
-		maxLength = max(self.sequenceLengths[self.cursor[i]:index])
-		x = self.data[self.cursor[i]:index, :maxLength, :-1]
-		y = self.data[self.cursor[i]:index, :maxLength, -1]
-		sequenceLengths = self.sequenceLengths[self.cursor[i]:index]
-		musks = self.musks[self.cursor[i]:index, :maxLength]
-		self.cursor[i] += n
-		return x, y, sequenceLengths, musks
+    def next_batch(self, n):
+        if np.any(self.cursor + n + 1 > self.size):
+            self.epochs += 1
+            self.cursor -= self.cursor
+        i = np.random.randint(0, self.numBuckets)
+        index = min(self.cursor[i] + n, self.size)
+        maxLength = max(self.sequenceLengths[self.cursor[i]:index])
+        x = self.data[self.cursor[i]:index, :maxLength, :-1]
+        y = self.data[self.cursor[i]:index, :maxLength, -1]
+        sequenceLengths = self.sequenceLengths[self.cursor[i]:index]
+        musks = self.musks[self.cursor[i]:index, :maxLength]
+        self.cursor[i] += n
+        return x, y, sequenceLengths, musks
